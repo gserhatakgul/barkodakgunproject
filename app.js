2eb7218b4b1c23927ea922314a253bc8deb93835
gserhatakgul <gserhatakgul@gmail.com>
2023-07-22 14:05:51 +0300

    get_all api tasarlandı barcode

--- app.js
+++ app.js
@@ -6,21 +6,31 @@ const pool = require("./db");
 
 app.use(express.json());
 
+// get all barkod
+app.get("/barkodproducts", async (req, res) => {
+  try {
+    const allbarcode = await pool.query("SELECT * FROM barkodproducts");
+    res.json(allbarcode.rows);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 // get barkod
 
 // update barkod
 
-// create barkod 
+// create barkod
 
 app.post("/barkodproducts", async (req, res) => {
   try {
     const { barcode, product_id, product_name, amount, production_date, unit } =
       req.body;
     const newbarcode = await pool.query(
-      "INSERT INTO barkodproducts (barcode,product_id,product_name,amount,production_date,unit) VALUES ($1,$2,$3,$4,$5,$6)",
+      "INSERT INTO barkodproducts (barcode,product_id,product_name,amount,production_date,unit) VALUES ($1,$2,$3,$4,$5,$6) RETURNING * ",
       [barcode, product_id, product_name, amount, production_date, unit]
     );
-    res.json(newbarcode.rows[1]);
+    res.json(newbarcode.rows[2]);
   } catch (error) {
     console.error(error);
   }
