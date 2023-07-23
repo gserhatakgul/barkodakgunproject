209bf21171c9208a834f3115efd61fbc0005cb65
gserhatakgul <gserhatakgul@gmail.com>
2023-07-23 15:20:29 +0300

    React eklendi

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React, { Fragment } from "react";
+import Barkodsorgulaekran from "./components/BarkodSorgula";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <Barkodsorgulaekran > </Barkodsorgulaekran>
+        </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/BarkodSorgula.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Barkodsorgulaekran = () => {
+  return <h1> MERHABA </h1>;
+};
+
+export default Barkodsorgulaekran;
