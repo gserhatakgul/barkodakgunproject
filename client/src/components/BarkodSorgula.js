55d48c64f3cbcf865f6c14bdb82df8efea9e4c69
gserhatakgul <gserhatakgul@gmail.com>
2023-07-24 13:15:06 +0300

    ÖN YÜZE BAŞLANDI BARKOD SORGULAMA EKRANI İÇİN TASARIM  YAPILDI

--- client/src/components/BarkodSorgula.js
+++ client/src/components/BarkodSorgula.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { Fragment,  } from "react";
 
 const Barkodsorgulaekran = () => {
-  return <h1> MERHABA </h1>;
+  //const { barcode, Setbarcode } = useState([]);
+
+  return (
+    <Fragment>
+      <h1 className="text-center text-primary my-5 ">
+        BARKOD SORGULAMA EKRANI{" "}
+      </h1>
+      <form className="d-flex ">
+        <input
+          type="text"
+          placeholder="barkod giriniz"
+          name="myInput"
+          className="form-control m-1"
+        />
+        <button type="button" class="btn btn-primary m-1 ">
+          SORGULA
+        </button>
+        <button type="button" class="btn btn-primary m-1">
+          TÜM BARKODLARI SORGULA
+        </button>
+      </form>
+    </Fragment>
+  );
 };
 
 export default Barkodsorgulaekran;
